2a8fc5c739a7c5c37b229271e7eed93151be1cc5
jiseong0612 <hth5545@gmail.com>
2023-12-17 01:02:00 +0900

    예제 연관관계 매핑 예시

---
+++ jpa-test/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,76 @@
+package jpabook.jpashop.domain;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+	@Id
+	@GeneratedValue
+	@Column(name = "ORDER_ID")
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "MEMBER_ID")
+	private Member member;
+
+	private LocalDateTime orderDate;
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+	
+	//1.주문서에서 주문한 아이템목록을 보고 싶을 때
+	@OneToMany(mappedBy = "order")
+	private List<OrderItem> orderItems = new ArrayList<OrderItem>();
+
+	//2.양방향 연관관계 순수 객체 대응 함수
+	public void addOrderItem(OrderItem orderItem) {
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+}
---
+++ jpa-test/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class OrderItem {
+	@Id
+	@GeneratedValue
+	@Column(name = "ORDER_ITEM_ID")
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "ORDER_ID")
+	private Order order;
+	
+	@ManyToOne
+	@JoinColumn(name = "ITEM_ID")
+	private Item item;
+	
+	private int orderprice;
+	private int count;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	public int getOrderprice() {
+		return orderprice;
+	}
+	public void setOrderprice(int orderprice) {
+		this.orderprice = orderprice;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+}
